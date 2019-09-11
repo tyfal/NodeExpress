62dd28cb7e92d076bd35dd4183a3adf0ad36a66b
tyfal <tfalcoff@gmail.com>
2019-09-11 03:30:46 -0500

    functionality for survey page

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,8 +1,51 @@
 var friendArray = [
     {
-        name:  `Tyler`,
-        photo: ``,
-        scores: []
+        name: `Harry Potter`,
+        photo: `https://img.maximummedia.ie/her_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtaGVyLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE5XFxcLzA5XFxcLzA2MTAxMTM4XFxcL2hhcnJ5LXBvdHRlci1zaG9ja2VkLWZhY2UtMTU0NzI0MzIwMy0xMDI0eDUxMi5qcGdcIixcIndpZHRoXCI6NzQwLFwiaGVpZ2h0XCI6NDE2LFwiZGVmYXVsdFwiOlwiaHR0cHM6XFxcL1xcXC93d3cuaGVyLmllXFxcL2Fzc2V0c1xcXC9pbWFnZXNcXFwvaGVyXFxcL25vLWltYWdlLnBuZz9pZD0wZDJkNjI3YzA1OWI5ZWRjYWIwZFwiLFwib3B0aW9uc1wiOltdfSIsImhhc2giOiI2ODJkODUyZGNlN2FiNGMzMjFiMmM2YTE5YWUxNWMwN2VhMmNkN2E5In0=/harry-potter-shocked-face-1547243203-1024x512.jpg`,
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Ronald Weasley",
+        "photo": "https://media.giphy.com/media/NJfyshe1cqEaA/giphy.gif",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Tyler",
+        "photo": "https://media.giphy.com/media/1Mng0gXC5Tpcs/giphy.gif",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
     }
 ];
 
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -8,4 +8,39 @@ module.exports = function(app) {
 
     });
 
+    app.post(`/api/friends`, function(req, res) {
+
+        friendData.push(req.body);
+
+        var bestMatch = {
+            friend: {},
+            diff: 10000
+        };
+
+        friendData.forEach(friend => {
+
+            var diff = 0;
+
+            if (friend !== req.body) {
+
+                for (var i = 0; i < friend.scores.length; i++) {
+
+                    diff += Math.abs(friend.scores[i] - req.body.scores[i]);
+
+                }
+
+                if (diff < bestMatch.diff) {
+
+                    bestMatch.friend = friend;
+                    bestMatch.diff = diff;
+
+                }
+
+            }
+        });
+
+        res.json(bestMatch.friend);
+
+    })
+
 }
